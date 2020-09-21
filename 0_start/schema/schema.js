1fda4f520c850d5f4ae9b24f1068a9e0865b40f1
ivanstepanov <ivanstepanoff@yahoo.com>
2020-09-21 08:17:41 +0200

    the Query & Mutauion are created

--- 0_start/schema/schema.js
+++ 0_start/schema/schema.js
@@ -1,33 +1,36 @@
 const graphql = require('graphql')
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql // ???
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList, GraphQLNonNull } = graphql // ???
+const Movies = require('../models/movie')
+const Directors = require('../models/director')
 
-const movies = [
-    {id:'1', name: 'Snatch', genre: 'Crime Comedy', directorId: 2},
-    {id:2, name: '1984', genre: 'Si-Fi', directorId: 1},
-    {id:'3', name: 'Pulp Fiction', genre: 'Crime', directorId: 4},
-    {id:4, name: 'V for vendetta', genre: 'Si-Fi Thriller', directorId: 3},
-    {id:'5', name: 'Reservour Dogs', genre: 'Crime', directorId: 4},
-    {id:6, name: 'The Hateful Eight', genre: 'Crime', directorId: 4},
-    {id:'7', name: 'Inglorious Basterds', genre: 'Crime', directorId: 4},
-    {id:8, name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime comedy', directorId: 2},
-]
-const directors = [
-    {id:1, name: 'Michael Redford', age: 72},
-    {id:'2', name: 'Guy Ritchie', age: 50},
-    {id:3, name: 'James McTeique', age: 51},
-    {id:'4', name: 'Quentin Tarantino', age: 55},
-]
+// const movies = [
+//     {id:'1', name: 'Snatch', genre: 'Crime Comedy', directorId: 2},
+//     {id:2, name: '1984', genre: 'Si-Fi', directorId: 1},
+//     {id:'3', name: 'Pulp Fiction', genre: 'Crime', directorId: 4},
+//     {id:4, name: 'V for vendetta', genre: 'Si-Fi Thriller', directorId: 3},
+//     {id:'5', name: 'Reservour Dogs', genre: 'Crime', directorId: 4},
+//     {id:6, name: 'The Hateful Eight', genre: 'Crime', directorId: 4},
+//     {id:'7', name: 'Inglorious Basterds', genre: 'Crime', directorId: 4},
+//     {id:8, name: 'Lock, Stock and Two Smoking Barrels', genre: 'Crime comedy', directorId: 2},
+// ]
+// const directors = [
+//     {id:1, name: 'Michael Redford', age: 72}, // 5f674ea824d6a77f12a59e89
+//     {id:'2', name: 'Guy Ritchie', age: 50}, // 5f674f2f24d6a77f12a59e8a
+//     {id:3, name: 'James McTeique', age: 51},//5f674fb624d6a77f12a59e8b
+//     {id:'4', name: 'Quentin Tarantino', age: 55},//5f6750a224d6a77f12a59e8c
+// ]
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
         id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
+        name: { type: new GraphQLNonNull(GraphQLString)},
+        genre: { type: new GraphQLNonNull(GraphQLString) },
         director: {
             type: DirectorType,
             resolve(parent, args) {
-                return directors.find(director => director.id == parent.directorId)
+                // return directors.find(director => director.id == parent.directorId)
+                return Directors.findById(parent.directorId)
             }
         }
     }),
@@ -36,15 +39,84 @@ const DirectorType = new GraphQLObjectType({
     name: 'Director',
     fields: () => ({
         id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies.filter(movie => movie.directorId == parent.id)
+                // return movies.filter(movie => movie.directorId == parent.id)
+                return Movies.find({directorId: parent.id})
             }
         }
     })
+})
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addMovie: {
+            type: MovieType,
+            args: { name: {type: new GraphQLNonNull(GraphQLString)}, genre: {type: new GraphQLNonNull(GraphQLString)}, directorId: {type: GraphQLID}},
+            resolve(parent, args) {
+                const movie = new Movies({
+                    // id: args.id,
+                    name: args.name,
+                    genre: args.genre,
+                    directorId: args.directorId
+                })
+                return movie.save()
+            }
+        },
+        addDirector: {
+            type: DirectorType,
+            args: {name: {type: new GraphQLNonNull(GraphQLString)}, age: {type: new GraphQLNonNull(GraphQLInt)}},
+            resolve(parent, args) {
+                const director = new Directors({
+                    name: args.name,
+                    age: args.age
+                })
+                return director.save()
+            }
+        },
+        deleteMovie: {
+            type: MovieType,
+            args: { id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return Movies.findByIdAndDelete(args.id)
+            }
+        },
+        deleteDirector: {
+            type: DirectorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return Directors.findByIdAndDelete(args.id)
+            }
+        },
+        updateMovie: {
+            type: MovieType,
+            args: { id: {type: GraphQLID}, name: {type: new GraphQLNonNull(GraphQLString)}, genre: {type: new GraphQLNonNull(GraphQLString)}, directorId: {type: GraphQLID} },
+
+            resolve(parent, args) {
+                return Movies.findByIdAndUpdate(
+                    args.id,
+                    {$set: {name: args.name, genre: args.genre} },
+                    {new: true}
+                )
+            }
+        },
+        updateDirector: {
+            type: DirectorType,
+            args: { id: {type: GraphQLID}, name: {type: new GraphQLNonNull(GraphQLString)}, age: {type: new GraphQLNonNull(GraphQLInt)} },
+
+            resolve(parent, args) {
+                return Directors.findByIdAndUpdate(
+                    args.id,
+                    {$set: {name: args.name, age: args.age} },
+                    {new: true}
+                )
+            }
+        },
+    }
 })
 
 const Query = new GraphQLObjectType({
@@ -54,32 +126,37 @@ const Query = new GraphQLObjectType({
             type: MovieType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-                return movies.find(movie => movie.id == args.id) // при работе с ID сравнение НЕстрогое!
+                // return movies.find(movie => movie.id == args.id) // при работе с ID сравнение НЕстрогое!
+                return Movies.findById(args.id)
             }
         },
         director: {
             type: DirectorType,
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-                return directors.find(director => director.id == args.id)
+                // return directors.find(director => director.id == args.id)
+                return Directors.findById(args.id)
             }
         },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies
+                // return movies
+                return Movies.find({})
             }
         },
         directors: {
             type: new GraphQLList(DirectorType),
             resolve(parent, args) {
-                return directors
+                // return directors
+                return Directors.find({})
             }
         }
     }
 })
 
 module.exports =  new GraphQLSchema({
-    query: Query
+    query: Query,
+    mutation: Mutation
 })
 
--- 0_start/server/app.js
+++ 0_start/server/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const {graphqlHTTP } = require('express-graphql')
 const schema = require('../schema/schema')
+const mongoose = require('mongoose')
 
 const PORT = 3005
 const app = express()
+
+mongoose.connect('mongodb+srv://hugomailru:2A6UtkuHeq%5ETca3@cluster0.q2h5r.mongodb.net/cinema?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connection.on('error', err => console.log(`Connection error: ${err}`))
+mongoose.connection.once('open', () => console.log('Connected to DB!!!'))
 
 app.use('/graphql', graphqlHTTP({ // {"errors":[{"message":"GraphQL middleware options must contain a schema."}]}
     schema,
@@ -13,3 +18,5 @@ app.use('/graphql', graphqlHTTP({ // {"errors":[{"message":"GraphQL middleware o
 app.listen(PORT, err => {
     err ? console.log(err): console.log('Server started!!!')
 })
+
+
